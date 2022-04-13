5ac64bedc7d372f0189ea35020d440031e254271
piyush awasthi <61102891+piyushawasthi-13@users.noreply.github.com>
2022-04-13 18:54:42 +0530

    Update views.py

--- project/app/views.py
+++ project/app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ok hello brother..")
+    return HttpResponse("ok hello Big brother..")
